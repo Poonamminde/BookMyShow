6b6114b9720ca5787ab7cf3e35ec1922f46462db
Poonam Minde <poonam@Mac.lan>
2026-03-18 14:11:29 +0530

    remove unused imports

--- frontend/src/pages/User/SeatsPage.tsx
+++ frontend/src/pages/User/SeatsPage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { api } from '../../services/api';
-import Card from '../../components/common/Card';
 
 type Seat = { id: number; seat_number: number; status: string };
 
